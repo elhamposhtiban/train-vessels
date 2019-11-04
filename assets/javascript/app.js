86bf87fdb9df47ff6afaaa5d27cd5dae66af05ce
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2019-11-04 14:43:31 -0600

    creating data base on firebase and add config and script tag to page

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyB8R0VWT1oesDwMBNdgTaVNHaBvqFFwMFM",
+    authDomain: "train-vessels.firebaseapp.com",
+    databaseURL: "https://train-vessels.firebaseio.com",
+    projectId: "train-vessels",
+    storageBucket: "train-vessels.appspot.com",
+    messagingSenderId: "708232030880",
+    appId: "1:708232030880:web:a40b6c70c66ad5ba3f37c5",
+    measurementId: "G-G89E4JE211"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
